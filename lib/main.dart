aa3c4cba1f05c88d6953b9acb69414cd29f9f207
yehezkieldio <yehezkieldio35@gmail.com>
2023-08-10 00:07:27 +0800

    ..already some stuff

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const ExamBrowserApp());
+}
+
+class ExamBrowserApp extends StatelessWidget {
+  const ExamBrowserApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CBT',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: WillPopScope(
+        onWillPop: () async {
+          return false;
+        },
+        child: const HomePage(title: 'CBT'),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _urlController = TextEditingController();
+
+  void despose() {
+    _urlController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        actions: const [AppPopupMenuButton()],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _urlController,
+              decoration: const InputDecoration(labelText: 'Enter URL'),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  final enteredUrl = _urlController.text;
+                  if (enteredUrl.isNotEmpty) {
+                    Uri? uri = Uri.tryParse(enteredUrl);
+                    if (uri != null &&
+                        (uri.scheme == 'http' || uri.scheme == 'https')) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BrowserScreen(
+                                    url: enteredUrl,
+                                  )));
+                    }
+                  }
+                },
+                child: const Text('Open URL'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BrowserScreen extends StatelessWidget {
+  final String url;
+
+  const BrowserScreen({required this.url, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+        ),
+      )
+      ..loadRequest(Uri.parse(url));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Browser'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        automaticallyImplyLeading: false,
+        actions: const [AppPopupMenuButton()],
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
+
+class AppPopupMenuButton extends StatelessWidget {
+  const AppPopupMenuButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      onSelected: (value) {
+        if (value == 'info') {
+          // handle info
+        } else if (value == 'exit') {
+          if (Platform.isAndroid) {
+            SystemNavigator.pop();
+          } else if (Platform.isIOS) {
+            exit(0);
+          }
+        }
+      },
+      itemBuilder: (BuildContext context) => [
+        const PopupMenuItem(value: 'info', child: Text('Info')),
+        const PopupMenuItem(value: 'exit', child: Text('Exit App'))
+      ],
+    );
+  }
+}
